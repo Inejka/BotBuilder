28004ea7284b0bbe6993f31f7a383afd02b310e6
Inejka <dante12654@gmail.com>
2023-02-20 17:30:28 +0300

    fix: fixed reversion of transits

--- bot/Transit.py
+++ bot/Transit.py
@@ -6,7 +6,7 @@ from utils.StrWrapper import StrWrapper
 
 @WithAssociatedFile
 class Transit:
-    def __init__(self, inner_id: int, to_state, from_state, priority: int):
+    def __init__(self, inner_id: int, from_state, to_state, priority: int):
         self.__inner_id = "transit_" + str(inner_id)
         self.__name = StrWrapper("Transit" + str(inner_id))
         self.__to_state = to_state
@@ -20,9 +20,8 @@ class Transit:
         return self.__name
 
     def to_json(self):
-        #json loads for bot serialization for not to return a str
+        # json loads for bot serialization for not to return a str
         # todo find way to remove json loads
         return json.loads('{"inner_id":"' + self.__inner_id + '","name":"' + self.__name.get() + '","to_state":"' \
-            + self.__to_state.get_id() + '","from_state":"' + self.__from_state.get_id() + '","priority":' + str(
-                self.__priority) + ',"associated_file_path":"' + self.get_associated_file() + '"}')
-
+                          + self.__to_state.get_id() + '","from_state":"' + self.__from_state.get_id() + '","priority":' + str(
+            self.__priority) + ',"associated_file_path":"' + self.get_associated_file() + '"}')
